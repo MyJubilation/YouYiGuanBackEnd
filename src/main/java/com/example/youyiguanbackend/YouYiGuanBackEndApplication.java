f042846457a867405f293745daf7ec6d2be3a346
GodOfWar-is <2515979610@qq.com>
2024-11-15 13:09:15 +0800

    创建了一些文件，方便协作

---
+++ src/main/java/com/example/youyiguanbackend/YouYiGuanBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.youyiguanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouYiGuanBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouYiGuanBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/youyiguanbackend/YouYiGuanBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.youyiguanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouYiGuanBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
