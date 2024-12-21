ac7ba7e28c6c9fb2e28f0ce76d3fb1363a5de3cc
beetles <2835807030@qq.com>
2024-12-21 18:46:32 +0800

    2024/12/21 完成医生功能 查看指定病人详细诊断信息

--- src/main/java/com/example/youyiguanbackend/models/doctor/controller/PatientController.java
+++ src/main/java/com/example/youyiguanbackend/models/doctor/controller/PatientController.java
@@ -2,6 +2,7 @@ package com.example.youyiguanbackend.models.doctor.controller;
 
 import com.example.youyiguanbackend.common.doctor.Result.Result;
 import com.example.youyiguanbackend.models.doctor.model.dto.PatientGetListDTO;
+import com.example.youyiguanbackend.models.doctor.model.pojo.GetPatientInfoByPatientIdVO;
 import com.example.youyiguanbackend.models.doctor.model.pojo.PatientGetListVO;
 import com.example.youyiguanbackend.models.doctor.model.pojo.PatientInfo;
 import com.example.youyiguanbackend.models.doctor.service.PatientService;
@@ -9,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 /**
@@ -63,6 +67,33 @@ public class PatientController {
             return Result.success(result);
         }else {
             return Result.error(1001,"获取失败，参数错误");
+        }
+    }
+    /**
+     * 获取指定病人诊断信息
+     */
+    @GetMapping("/{patient_id}/medical-records")
+    public Result<?> getPatientInfoByPatientId(@PathVariable int patient_id,
+                                               @RequestParam(value = "review_status", required = false) String review_status,
+                                               @RequestParam(value = "patient_status", required = false) String patient_status,
+                                               @RequestParam(value = "date_from", required = false) String Bdate_from,
+                                               @RequestParam(value = "date_to", required = false) String Bdate_to){
+        LocalDateTime date_from = null;
+        LocalDateTime date_to = null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        if(Bdate_from != null){
+            LocalDate localDateFrom = LocalDate.parse(Bdate_from, formatter);
+            date_from = localDateFrom.atStartOfDay(); // 将LocalDate转换为LocalDateTime，时间为00:00:00
+        }
+        if(Bdate_to != null){
+            LocalDate localDateTo = LocalDate.parse(Bdate_to, formatter);
+            date_to = localDateTo.atStartOfDay().plusDays(1).minusNanos(1); // 将LocalDate转换为LocalDateTime，时间为23:59:59.999999999
+        }
+        List<GetPatientInfoByPatientIdVO> list = patientService.getPatientInfoByPatientId(patient_id,review_status,patient_status,date_from,date_to);
+        if(!list.isEmpty()){
+            return Result.success(list);
+        }else {
+            return Result.error(401,"该病人没有关联的诊断记录");
         }
     }
 }
